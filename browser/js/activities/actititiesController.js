4f389429ab5fa3b78f6c102e37d15d2251458207
Felipe Caputo <felipe.caputo@gmail.com>
2015-10-27 00:01:06 -0200

    Changed the way we create new activities

--- browser/js/activities/actititiesController.js
+++ browser/js/activities/actititiesController.js
@@ -1,20 +1,6 @@
-angular.module('timeApp').controller('activitiesController', function ($scope, Activity, activityListService, $modal){
+angular.module('timeApp').controller('activitiesController', function ($scope, Activity, activityListService){
   $scope.activityListService  = activityListService;
   $scope.visualization = 'card';
-
-  $scope.newActivityOld = function () {
-    var _activity = new Activity();
-    var modalInstace = $modal.open({
-      animation: true,
-      template: 'js/activity/modalTemplate.html',
-      controller: 'activityModalController',
-      'resolve': {
-        activity: function () {
-          return _activity;
-        }
-      }
-    })
-  };
 
   $scope.editActivity = function (activity) {
 
--- browser/js/activity/activityController.js
+++ browser/js/activity/activityController.js
@@ -1,5 +1,10 @@
 angular.module('timeApp').controller('activityController', function ($scope, Activity, activityListService, $routeParams, configService){
-  $scope.activity = activityListService.getActivity($routeParams.activityId);
+  if ($routeParams.activityId == 'new') {
+    $scope.activity = new Activity();
+  } else {
+    $scope.activity = activityListService.getActivity($routeParams.activityId);
+  }
+
   $scope.config = configService;
 
   $scope.startCalendarOpened = false;
